2f6fadf8237586ded3b35e967a7edaaf8a73ca78
Łukasz Kluge <kluge.lukasz@gmail.com>
2023-10-31 11:50:05 +0100

    Comments added, JwtAuthToken finished

--- JwtAuthToken7.0/Controllers/AuthController.cs
+++ JwtAuthToken7.0/Controllers/AuthController.cs
@@ -14,9 +14,12 @@ namespace JwtAuthToken7._0.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        // Create a user variable to be used within the controller
+
         public static User user = new User();
         private readonly IConfiguration _configuration;
 
+        // Controller constructor that takes configuration as a parameter
         public AuthController(IConfiguration configuration)
         {
             _configuration = configuration;
@@ -26,8 +29,12 @@ namespace JwtAuthToken7._0.Controllers
 
         public ActionResult<User> Register(UserRequest request)
         {
+            // Hash the user's password before storing it in the database
+            
             string passwordHash
                 = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            // Set the username and password hash in the user object
 
             user.Username = request.Username;
             user.PasswordHash = passwordHash;
@@ -39,38 +46,54 @@ namespace JwtAuthToken7._0.Controllers
 
         public ActionResult<User> Login(UserRequest request)
         {
+            // Check if a user with the given username exists
+
             if (user.Username != request.Username)
             {
                 return BadRequest("User not found.");
             }
+
+            // Verify the password provided by the user
 
             if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
             {
                 return BadRequest("Wrong password.");
             }
 
+            // Create a JWT token for user authentication
+
             string token = CreateToken(user);
 
             return Ok(token);
         }
 
-        // NEED SMALL CHANGES
+        // Function to generate a JWT token
         private string CreateToken(User user)
         {
+            // Create a list of claims, typically containing user data
+
             List<Claim> claims = new List<Claim> { 
                 new Claim(ClaimTypes.Name, user.Username)
             };
 
+            // Create a key used for signing the token
+
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
                 _configuration.GetSection("AppSettings:Token").Value!));
 
+            // Create the credentials needed for token signing
+
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            // Create a JWT token with specified claims and signing data
 
             var token = new JwtSecurityToken(
                 claims: claims,
                 expires: DateTime.Now.AddDays(1),
                 signingCredentials: creds
                 );
+
+            // Create and return a JWT string
 
             var jwt = new JwtSecurityTokenHandler().WriteToken(token);
 
